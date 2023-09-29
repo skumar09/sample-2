479b5ba652b0ddb337c515dc1300b453f0ad66c2
nateekar <nateekar@adobe.com>
2023-09-28 17:15:01 -0700

    update

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,9 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+  ? [['github'], ['list'], ['html']]
+  : [['html', { outputFolder: 'test-html-results' }],['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
